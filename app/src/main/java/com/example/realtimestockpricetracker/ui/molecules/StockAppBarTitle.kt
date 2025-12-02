0ef88843787bc22acb53c2b94233312ea2e50d0e
shaik <shaikhfaizanfjh35@gmail.com>
2025-12-02 19:09:44 +0400

    Stock App Bar Title Updated with Atom

--- app/src/main/java/com/example/realtimestockpricetracker/ui/molecules/StockAppBarTitle.kt
+++ app/src/main/java/com/example/realtimestockpricetracker/ui/molecules/StockAppBarTitle.kt
@@ -1,27 +1,18 @@
 package com.example.realtimestockpricetracker.ui.molecules
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import com.example.realtimestockpricetracker.ui.atoms.StatusDot
 
 @Composable
 fun StockAppBarTitle(isConnected: Boolean) {
     Row(verticalAlignment = Alignment.CenterVertically) {
-        Box(
-            modifier = Modifier
-                .size(12.dp)
-                .background(
-                    color = if (isConnected) Color.Green else Color.Red,
-                    shape = CircleShape
-                )
-        )
+        StatusDot(isConnected)
         Spacer(Modifier.width(8.dp))
         Text(
             "Stock Price Tracker",
