c00fd82851078d97f37ccffa644c89fda48312d1
shaik <shaikhfaizanfjh35@gmail.com>
2025-12-02 22:31:36 +0400

    Updated pass test case

--- app/src/test/java/com/example/realtimestockpricetracker/RealTimeStockViewModelTest.kt
+++ app/src/test/java/com/example/realtimestockpricetracker/RealTimeStockViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.example.realtimestockpricetracker
 
 import app.cash.turbine.test
+import com.example.realtimestockpricetracker.data.StockFeedRepository
+import com.example.realtimestockpricetracker.model.StockUpdateDto
 import com.example.realtimestockpricetracker.viewmodel.RealTimeStockViewModel
 import com.example.realtimestockpricetracker.ws.WebSocketManager
 import io.mockk.every
@@ -9,10 +11,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -21,9 +25,7 @@ import org.junit.Test
 class RealTimeStockViewModelTest {
 
     private val testDispatcher = StandardTestDispatcher()
-
-    private val wsFlow = MutableSharedFlow<String>()
-
+    private val wsFlow = MutableSharedFlow<String>(replay = 1)
     private lateinit var viewModel: RealTimeStockViewModel
 
     @Before
@@ -31,10 +33,11 @@ class RealTimeStockViewModelTest {
         Dispatchers.setMain(testDispatcher)
 
         val mockWs = mockk<WebSocketManager>(relaxed = true)
+        val mockRepo = mockk<StockFeedRepository>(relaxed = true)
 
         every { mockWs.incomingMessages() } returns wsFlow
 
-        viewModel = RealTimeStockViewModel(mockWs)
+        viewModel = RealTimeStockViewModel(mockWs, mockRepo)
     }
 
     @After
@@ -45,23 +48,21 @@ class RealTimeStockViewModelTest {
     @Test
     fun `emit messages updates state`() = runTest {
         viewModel.toggleFeed()
-        advanceUntilIdle()
-
-        wsFlow.emit("AAPL|150.0")
-        wsFlow.emit("GOOG|2500.0")
 
         viewModel.state.test {
-            skipItems(1) // initial state
+            skipItems(1)
+
+            wsFlow.emit(Json.encodeToString(StockUpdateDto("AAPL", 150.0)))
+            runCurrent()
 
             val s1 = awaitItem()
             assert(s1.stocks.any { it.symbol == "AAPL" && it.price == 150.0 })
+
+            wsFlow.emit(Json.encodeToString(StockUpdateDto("GOOG", 2500.0)))
+            runCurrent()
 
             val s2 = awaitItem()
             assert(s2.stocks.any { it.symbol == "GOOG" && it.price == 2500.0 })
         }
     }
 }
-
-
-
-
